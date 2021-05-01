893cb47d43248a6ec887d9a1f4d0e0e0118825c6
NITESH AKHOURI <nitesh@NITESHs-MacBook-Air.local>
2021-05-02 03:38:05 +0530

    added db changes

--- src/main/java/com/company/application/controller/DBController.java
+++ src/main/java/com/company/application/controller/DBController.java
@@ -2,6 +2,7 @@ package com.company.application.controller;
 
 import com.company.application.model.Candidate;
 import com.company.application.repository.CandidateRepository;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,9 @@ public class DBController {
     @RequestMapping("/test_db")
     public String test_DB_Connection(){
         List<Candidate> listCandidates=candidateRepository.findAll();
+        System.out.println(listCandidates);
         Candidate candidate=listCandidates.get(0);
-        return candidate.toString();
+        System.out.println(candidate.getName());
+        return candidate.getName();
     }
 }
--- src/main/java/com/company/application/repository/CandidateRepository.java
+++ src/main/java/com/company/application/repository/CandidateRepository.java
@@ -2,9 +2,14 @@ package com.company.application.repository;
 
 import com.company.application.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
 
+    @Query("select * from slack_developers.CANDIDATE_DETAILS")
+    List<Candidate> findAll();
 }
